4d86cb629977da74da898328cfe2758b44e16619
Allendale Nato <natoallendale@gmail.com>
2017-08-03 20:16:19 +0800

    Modify draw card to module to return both with and without replacement.

---
+++ app/modules/drawCard.js
@@ -0,0 +1,29 @@
+/**
+ * Draw n number of cards per each N trials in a standard deck.
+ */
+const R = require('r-script');
+
+/**
+ * Returns a list containing the result of the experiment both with and without replacement.
+ * 
+ * @module
+ * @param {Number} N A positive integer that represents the N number of trials.
+ * @param {Number} n A positive integer that represents the n number of cards to draw.
+ */
+const drawCard = (N, n) => {
+    let result = {};
+
+    // Generate Experiment with replacement.
+    result.withReplacement = R('./../../scripts/draw-card.R')
+    .data(N, n, true)
+    .callSync();
+
+    // Generate Experiment without replacment
+    result.withoutReplacement = R('./../../scripts/draw-card.R')
+    .data(N, n, false)
+    .callSync();
+
+    return result;
+};
+
+module.exports = drawCard;
