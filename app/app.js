bd2ac60b8d209752b40ea86bcaaf5df0dfa4bfd2
Allendale Nato <natoallendale@gmail.com>
2017-08-04 01:14:20 +0800

    Add JSON logging module

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 const drawCard = require('./modules/drawCard');
 const parseExperiment = require('./modules/parseExperiment');
+const log = require('./modules/logResult');
 
 let rawRes = drawCard(3, 5);
 let parsedRes = parseExperiment(rawRes);
+
+log.raw(rawRes);
+log.parsed(parsedRes);
--- app/modules/logResult.js
+++ app/modules/logResult.js
@@ -1,12 +1,30 @@
-const logRaw = (raw) => {
+const fs = require('fs');
 
+// Check if the logs directory exists.
+if (!fs.existsSync('./logs')) {
+    fs.mkdir('./logs', err => {
+        if (err) throw err;
+        console.log('Created ./logs directory');
+    });
+}
+
+const raw = (raw) => {
+    let rawJSON = JSON.stringify(raw, null, 2);
+    fs.writeFile('logs/raw_simulation.json', rawJSON, err => {
+        if (err) throw err;
+        console.log('Logged raw simulation');
+    });
 };
 
-const logParse = (parse) => {
-
+const parsed = (parse) => {
+    let parseJSON = JSON.stringify(parse, null, 2);
+    fs.writeFile('logs/parsed_simulation.json', parseJSON, err => {
+        if (err) throw err;
+        console.log('Logged parsed simulation');
+    });
 };
 
 module.exports = {
-    logRaw,
-    logParse
+    raw,
+    parsed
 };
