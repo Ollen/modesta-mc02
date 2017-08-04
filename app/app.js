4a04c66f452ebd69b4c7626938c19c43a0325b63
Allendale Nato <natoallendale@gmail.com>
2017-08-04 11:56:53 +0800

    Add electron boilerplate

---
+++ app/app.js
@@ -0,0 +1,45 @@
+const electron = require('electron');
+const app = electron.app;
+
+let mainWindow;
+
+function createWindow() {
+    const win = new electron.BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 800,
+        minHeight: 600,
+        title: 'Card Drawing Simulation',
+        center: true,
+        show: false
+    });
+
+    win.loadURL(`file://${__dirname}/views/test.html`);
+    win.on('closed', onClosed);
+    win.on('ready-to-show', () => {
+        win.show();
+    });
+
+    return win;
+}
+
+function onClosed() {
+    mainWindow = null;
+}
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (!mainWindow) {
+        mainWindow = createWindow();
+    }
+});
+
+app.on('ready', () => {
+    mainWindow = createWindow();
+});
+
