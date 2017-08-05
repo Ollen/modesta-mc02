0c67b64f93b1ae1531942397f6e623d10af03c0f
Allendale Nato <natoallendale@gmail.com>
2017-08-05 16:39:29 +0800

    Add JQuery validation in input page

--- app/views/js/input.js
+++ app/views/js/input.js
@@ -1,1 +1,41 @@
+let importLocation;
 
+$('#tempInput').click(() => {
+    $('#importFile').click();
+});
+
+$('#importFile').change(() => {
+    let fileLocation = document.getElementById('importFile').files[0].path;
+    let filename = fileLocation.split('\\');
+    $('#tempInput').val(filename[filename.length - 1]);
+    importLocation = fileLocation;
+});
+
+$('#importBtn').click(() => {
+    if (importLocation == null) {
+        notify('Invalid Input');
+        return;
+    }
+});
+
+$('#startBtn').click(() => {
+    let trialInput = $('#trialInput').val();
+    let drawInput = $('#drawInput').val();
+
+    if(trialInput < 10 || trialInput > 100000) {
+        notify('Invalid Input');
+        return;
+    }
+
+    if(drawInput < 1 || drawInput > 5) {
+        notify('Invalid Input');
+        return;
+    }
+
+    // Start Simulation Code Starts Here
+});
+
+
+function notify(message) {
+    alert(message);
+}
